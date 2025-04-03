25f0f57de842e7e4e71846b6958dce3248c5b274
Aluno <aluno@facens.br>
2025-04-03 20:15:26 -0300

    feat: Finalizando projeto

--- CalculoMedia/CalculoMedia.java
+++ CalculoMedia/CalculoMedia.java
@@ -8,43 +8,18 @@ public class CalculoMedia {
 
         System.out.println("Digite a nota mínima");
         notaMinima = scanner.nextDouble();
-        if (notaMinima < 0 || notaMinima > 10) {
-            System.out.println("Erro: Nota mínima inválida. O programa foi encerrado.");
-            scanner.close();
-            return;
-        }
 
         System.out.println("Digite a nota da AC1: ");
         ac1 = scanner.nextDouble();
-        if (ac1 < 0 || ac1 > 10) {
-            System.out.println("Erro: Nota AC1 inválida. O programa foi encerrado.");
-            scanner.close();
-            return; 
-        }
 
         System.out.println("Digite a nota da AC2: ");
         ac2 = scanner.nextDouble();
-        if (ac2 < 0 || ac2 > 10) {
-            System.out.println("Erro: Nota AC2 inválida. O programa foi encerrado.");
-            scanner.close();
-            return; 
-        }
 
         System.out.println("Digite a nota da AG: ");    
         ag = scanner.nextDouble();
-        if (ag < 0 || ag > 10) {
-            System.out.println("Erro: Nota AG inválida. O programa foi encerrado.");
-            scanner.close();
-            return;
-        }
 
         System.out.println("Digite a nota da AF: ");
         af = scanner.nextDouble();
-        if (af < 0 || af > 10) {
-            System.out.println("Erro: Nota AF inválida. O programa foi encerrado.");
-            scanner.close();
-            return;
-        }
 
         mediaFinal = (ac1 * 0.15) + (ac2 * 0.30) + (ag * 0.10) + (af * 0.45);
 
